0adb802083e21b95f5fdd05f93dc7180f5eeea83
Stephen Stack <sstack@punchthrough.com>
2016-03-10 15:39:28 -0600

    test: add new test sketches

---
+++ test/sketches/sketch_blue.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Bean.setLed(0, 0, 0);
+}
+
+void loop() {
+    Bean.setLedBlue(255);
+    Bean.sleep(2000);
+}
---
+++ test/sketches/sketch_red.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Bean.setLed(0, 0, 0);
+}
+
+void loop() {
+    Bean.setLedRed(255);
+    Bean.sleep(2000);
+}
